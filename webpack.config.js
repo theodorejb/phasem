e5e7cca2a39a01f0299cd0a79a174a74fc5b4a2b
Theodore Brown <theodorejb@outlook.com>
2018-10-18 21:33:54 -0500

    Update to Angular 7 and TypeScript 3.1, and enable the Angular Build Optimizer
    Minified bundle size decreased from 492 KB to 373 KB.

--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,54 @@ var HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 var path = require('path');
 
-module.exports = function(env) {
-    return [
-        {
-            entry: {
-                app: './compiled_ts/app/main.js',
-            },
-            output: {
-                filename: 'dist/[name].js',
-                path: path.resolve(__dirname, './public'),
-                publicPath: '',
-            },
-            optimization: {
-                minimizer: [
-                    new UglifyJsPlugin({
-                        extractComments: true,
-                    }),
-                ],
-            },
-            plugins: [
-                new HtmlWebpackPlugin({
-                    template: 'public/index_template.html',
-                    hash: true,
-                }),
-                new HtmlWebpackIncludeAssetsPlugin({
-                    assets: [
-                        'css/base_styles.css',
-                        'dist/js/core-js/shim.min.js',
-                        'dist/js/zone.js/zone.min.js',
-                    ],
-                    append: false,
-                    hash: true,
-                }),
+var config = {
+    entry: {
+        app: './compiled_ts/app/main.js',
+    },
+    output: {
+        filename: 'dist/[name].js',
+        path: path.resolve(__dirname, './public'),
+        publicPath: '',
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                extractComments: true,
+            }),
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index_template.html',
+            hash: true,
+        }),
+        new HtmlWebpackIncludeAssetsPlugin({
+            assets: [
+                'css/base_styles.css',
+                'dist/js/core-js/shim.min.js',
+                'dist/js/zone.js/zone.min.js',
             ],
-        },
-    ];
+            append: false,
+            hash: true,
+        }),
+    ],
+};
+
+module.exports = (env, argv) => {
+	// only enable Angular Build Optimizer in production mode to keep development builds fast
+    if (argv.mode === 'production') {
+        config.module = {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: '@angular-devkit/build-optimizer/webpack-loader',
+                    options: {
+                        sourceMap: false,
+                    },
+                },
+            ],
+        };
+    }
+
+    return config;
 };
