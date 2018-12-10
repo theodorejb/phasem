def11b831bf189363dadf63370434b0ed2007663
Theodore Brown <theodorejb@outlook.com>
2018-12-09 23:24:44 -0600

    Log API errors to standard PHP error log

--- api/errorHandlers.php
+++ api/errorHandlers.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Phasem\api;
 
+use Phasem\App;
 use Slim\Http\{Request, Response};
 use Teapot\{HttpException, StatusCode};
 
@@ -37,6 +38,71 @@ $container['errorHandler'] = function () {
         } else {
             $status = StatusCode::INTERNAL_SERVER_ERROR;
         }
+
+        $message = $e->getMessage();
+        $serverParams = $request->getServerParams();
+
+        if (!empty($serverParams['REMOTE_ADDR'])) {
+            $message .= ' | ip: ' . $serverParams['REMOTE_ADDR'];
+        }
+
+        $headers = $request->getHeaders();
+        $ignore = [
+            'CONTENT_LENGTH',
+            'HTTP_ACCEPT_LANGUAGE',
+            'HTTP_ACCEPT_ENCODING',
+            'HTTP_ACCEPT',
+            'HTTP_AUTHORIZATION',
+            'HTTP_CONNECTION',
+            'HTTP_COOKIE',
+            'HTTP_DNT',
+        ];
+
+        foreach ($ignore as $header) {
+            unset($headers[$header]);
+        }
+
+        if (count($headers) !== 0) {
+            $message .= ' | headers: ' . json_encode($headers);
+        }
+
+        $cookies = $request->getCookieParams();
+        unset($cookies['ApiAuth']);
+
+        if (count($cookies) !== 0) {
+            $message .= ' | cookies: ' . json_encode($cookies);
+        }
+
+        $body = $request->getBody()->getContents();
+        $jsonType = 'application/json';
+
+        // if the body has a JSON content-type, and the JSON contains a password property, don't log the password
+        if (substr($request->getContentType(), 0, strlen($jsonType)) === $jsonType) {
+            $decoded = json_decode($body);
+
+            if (is_object($decoded)) {
+                // hash value of any top-level property with "password" in its name
+
+                foreach ($decoded as $prop => $val) {
+                    if (strpos(strtolower($prop), 'password') !== false) {
+                        $decoded->$prop = sha1($val);
+                    }
+                }
+            }
+
+            $body = json_encode($decoded);
+        }
+
+        $message .= ' | endpoint: ' . $request->getUri()->__toString();
+        $message .= ' | method: ' . $request->getMethod();
+        $message .= ' | body: ' . $body;
+        $user = App::getUser();
+
+        if ($user !== null) {
+            $message .= ' | user: ' . $user->getEmail() . ' (#' . $user->getId() . ')';
+        }
+
+        error_log($message);
 
         return $response
             ->withStatus($status)
