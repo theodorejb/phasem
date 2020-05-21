4fd55eb3be9379e7cb680ebb1f3bc590d1970a15
Theodore Brown <theodorejb@outlook.com>
2020-05-20 19:57:14 -0600

    Update server setup for Ubuntu 20.04 and PHP 7.4

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -1,41 +1,49 @@
-# starting with Ubuntu 18.04 x64 image
+# starting with Ubuntu 20.04 x64 image
 
 # Add PHP ppa and install PHP
 sudo add-apt-repository ppa:ondrej/php
-sudo apt-get update
-sudo apt-get install php7.3-fpm
-sudo apt-get install php7.3-xml
-sudo apt-get install php7.3-mysql
-sudo apt-get install php7.3-mbstring
+sudo apt-get install php7.4-fpm php7.4-xml php7.4-mysql php7.4-mbstring
 
-# install nginx and unzip
-apt install nginx
-apt install unzip
+# install nginx and zip/unzip
+sudo apt install nginx
+sudo apt install zip unzip
+
+# set up email
+sudo apt install postfix
+# choose "Internet Site" for mail configuration.
 
 # install MySQL 8
 # see https://www.digitalocean.com/community/tutorials/how-to-install-the-latest-mysql-on-ubuntu-18-04
 cd /tmp
 # get link from https://dev.mysql.com/downloads/repo/apt/
-curl -OL https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
+curl -OL https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
 sudo dpkg -i mysql-apt-config*
 # arrow down to OK to keep defaults
 sudo apt update
 rm mysql-apt-config* # cleanup
 sudo apt install mysql-server
-# after setting root password, select mysql_native_password password hashing method since PHP doesn't support caching_sha2_password yet
+# after setting root password, select option to use strong password encryption
 mysql_secure_installation # yes to everything
 
 # remove symlink to disable default site
 rm /etc/nginx/sites-enabled/default
 
 # create/deploy initial production build
-npm run build-prod && npm run deploy
+npm run build-prod && npm run deploy # run locally, not on server
 
 # set up MySQL user, database, and schema
 mysql -u root -p
+
+CREATE DATABASE phasem CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 CREATE USER 'phasem'@'localhost' IDENTIFIED BY ''; # replace empty string with real password
+GRANT ALL PRIVILEGES ON phasem.* TO 'phasem'@'localhost';
+
+# if creating new instance of app:
 source /var/www/phasem/schema.sql;
-quit
+quit # exit MySQL
+
+# otherwise restore from backup:
+mysql -u [username] -p phasem < phasemdb_[date].sql
 
 # configure site
 cd /var/www/phasem
@@ -45,13 +53,12 @@ nano config.user.php
 
 # enable site
 ln -sfn /etc/nginx/sites-available/phasem /etc/nginx/sites-enabled/
-sudo service nginx restart
+sudo systemctl reload nginx
 
-# enable HTTPS - see https://certbot.eff.org/lets-encrypt/ubuntubionic-nginx
+# enable HTTPS - see https://certbot.eff.org/lets-encrypt/ubuntufocal-nginx
 sudo apt-get update
 sudo apt-get install software-properties-common
 sudo add-apt-repository universe
-sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
-sudo apt-get install certbot python-certbot-nginx
+sudo apt-get install certbot python3-certbot-nginx
 sudo certbot --nginx
